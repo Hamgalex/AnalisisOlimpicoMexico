5fec6fb7b3e278d6b8856400916cef87007cc71c
Hamgalex <hamgalex@hotmail.com>
2022-02-15 16:22:46 -0600

    Se añadio la gráfica de medallas por año

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from cmath import nan
 import streamlit as st
 import pandas as pd
 import numpy as np
-import seaborn as sns
 from matplotlib import pyplot as plt
 
 # Leer csv's
@@ -25,6 +24,8 @@ st.header('Lista de atletas mexicanos por temporada')
 datos_mexico_temporada=datos_mexico[datos_mexico.Season=="Summer"]    
 st.dataframe(datos_mexico_temporada)
 
+#
+#
 # Graficar las medallas por cada deporte
 st.header('Medallas por deporte')
 
@@ -32,18 +33,35 @@ medalla_oro=datos_mexico_temporada[datos_mexico_temporada.Medal=='Gold']
 medalla_plata=datos_mexico_temporada[datos_mexico_temporada.Medal=='Silver']
 medalla_bronce=datos_mexico_temporada[datos_mexico_temporada.Medal=='Bronze']
 
-deportes_medalla_oro=medalla_oro.Event.value_counts().reset_index(name='Gold')
-deportes_medalla_plata=medalla_plata.Event.value_counts().reset_index(name='Silver')
-deportes_medalla_bronce=medalla_bronce.Event.value_counts().reset_index(name='Bronze')
+deportes_medalla_oro=medalla_oro.Sport.value_counts().reset_index(name='Gold')
+deportes_medalla_plata=medalla_plata.Sport.value_counts().reset_index(name='Silver')
+deportes_medalla_bronce=medalla_bronce.Sport.value_counts().reset_index(name='Bronze')
 
 medallas=deportes_medalla_oro.set_index('index').join(deportes_medalla_plata.set_index('index'), how='outer').join(deportes_medalla_bronce.set_index('index'), how='outer')
 
-fig = plt.figure(figsize = (10, 4))
-plt.xticks(rotation='vertical')
-p1 = plt.bar(medallas.index, medallas.Gold,color="gold")
-p2= plt.bar(medallas.index, medallas.Silver,color="silver")
-p3= plt.bar(medallas.index, medallas.Bronze,color="goldenrod")
+fig = plt.figure(figsize = (6, 8))
+p1 = plt.barh(medallas.index, medallas.Gold,color="gold")
+p2= plt.barh(medallas.index, medallas.Silver,color="silver")
+p3= plt.barh(medallas.index, medallas.Bronze,color="goldenrod")
 
 plt.legend((p1[0], p2[0],p3[0]), ('Oro', 'Plata','Bronce'))
+st.pyplot(fig)
 
+#
+#
+# Graficar las medallas por año
+st.header('Medallas por año')
+
+año_medalla_oro=medalla_oro.Year.value_counts().reset_index(name='Gold')
+año_medalla_plata=medalla_plata.Year.value_counts().reset_index(name='Silver')
+año_medalla_bronce=medalla_bronce.Year.value_counts().reset_index(name='Bronze')
+
+años=año_medalla_oro.set_index('index').join(año_medalla_plata.set_index('index'), how='outer').join(año_medalla_bronce.set_index('index'), how='outer')
+
+fig = plt.figure(figsize = (10, 5))
+p1 = plt.bar(años.index, años.Gold,color="gold")
+p2= plt.bar(años.index, años.Silver,color="silver")
+p3= plt.bar(años.index, años.Bronze,color="goldenrod")
+
+plt.legend((p1[0], p2[0],p3[0]), ('Oro', 'Plata','Bronce'))
 st.pyplot(fig)
