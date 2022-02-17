bfbb84991ab788d4be0263ecfb78f31ad7453a00
Hamgalex <hamgalex@hotmail.com>
2022-02-17 11:01:14 -0600

    Se añadió la gráfica de medallas por año en linechart

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 # Leer csv's
-atletas = pd.read_csv("datasets/athlete_events.csv")
-regiones = pd.read_csv("datasets/noc_regions.csv")
+atletas = pd.read_csv("datasets/athlete_events.csv",encoding='utf-8')
+regiones = pd.read_csv("datasets/noc_regions.csv",encoding='utf-8')
 
 # Mergear los dos dataframe
 datos = pd.merge(atletas, regiones, on='NOC', how='left')
-
+datos.replace(to_replace="é", value="e")
 # Obtener datos México
 datos_mexico=datos[(datos.Team == 'Mexico')]
 
@@ -43,25 +43,23 @@ fig = plt.figure(figsize = (6, 8))
 p1 = plt.barh(medallas.index, medallas.Gold,color="gold")
 p2= plt.barh(medallas.index, medallas.Silver,color="silver")
 p3= plt.barh(medallas.index, medallas.Bronze,color="goldenrod")
-
+plt.xlabel('Número de Medallas', fontweight ='bold', fontsize = 15)
+plt.ylabel('Deporte', fontweight ='bold', fontsize = 15)
 plt.legend((p1[0], p2[0],p3[0]), ('Oro', 'Plata','Bronce'))
 st.pyplot(fig)
+
+
 
 #
 #
 # Graficar las medallas por año
 st.header('Medallas por año')
-
-año_medalla_oro=medalla_oro.Year.value_counts().reset_index(name='Gold')
-año_medalla_plata=medalla_plata.Year.value_counts().reset_index(name='Silver')
-año_medalla_bronce=medalla_bronce.Year.value_counts().reset_index(name='Bronze')
-
-años=año_medalla_oro.set_index('index').join(año_medalla_plata.set_index('index'), how='outer').join(año_medalla_bronce.set_index('index'), how='outer')
+medallas=['Gold','Silver','Bronze']
+año_con_medallas=datos_mexico_temporada[datos_mexico_temporada.Medal.isin(medallas)]
+años=año_con_medallas.Year.value_counts().reset_index(name='Year').set_index('index')
+años.sort_values(by="index", inplace=True)
+# st.dataframe(años)
 
 fig = plt.figure(figsize = (10, 5))
-p1 = plt.bar(años.index, años.Gold,color="gold")
-p2= plt.bar(años.index, años.Silver,color="silver")
-p3= plt.bar(años.index, años.Bronze,color="goldenrod")
-
-plt.legend((p1[0], p2[0],p3[0]), ('Oro', 'Plata','Bronce'))
+plt.plot(años.index,años.Year)
 st.pyplot(fig)
